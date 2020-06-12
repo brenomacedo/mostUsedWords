c4d48d11e9b5e383d3dbc804d9b7b81a48a1a6ee
brenosekiro <b88660501@gmail.com>
2020-06-12 01:12:27 -0300

    filteringRows

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,5 +1,6 @@
 const { ipcMain } = require('electron')
 const pathsToRows = require('./pathsToRows')
+const prepareData = require('./prepareData')
 ipcMain.on('process-subtitle', (event, paths) => {
     pathsToRows(paths).then(rows => console.log(rows)).then(() => {
         event.reply('process-subtitle', 'pong')
