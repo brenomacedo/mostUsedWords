5d576a842f503e9e51f4ba9a3bc084cc9400a885
brenosekiro <b88660501@gmail.com>
2020-06-11 23:13:29 -0300

    selectingCaptions

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <v-container fluid>
+      <v-form>
+          <v-file-input multiple chips v-model="files" label="Selecione as legendas"
+          prepend-icon="mdi-message-text" outlined append-outer-icon="mdi-send"
+          @click:append-outer="processSubtitles"></v-file-input>
+      </v-form>
       <div class='pills'>
           <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
       </div>
@@ -12,11 +17,17 @@ export default {
     components: { Pill },
     data: function() {
         return {
+            files: [],
             groupedWords: [
                 { name: 'you', amount: 900 },
                 { name: 'he', amount: 568 },
                 { name: 'i', amount: 1238 },
             ]
+        }
+    },
+    methods: {
+        processSubtitles() {
+            console.log(this.files)
         }
     }
 }
