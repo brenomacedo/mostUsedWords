5c5a552e000e1eb5ff4df29f98f63463c4b6126e
brenosekiro <b88660501@gmail.com>
2020-06-12 01:24:08 -0300

    groupingWords

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,10 +1,14 @@
 const { ipcMain } = require('electron')
 const pathsToRows = require('./pathsToRows')
 const prepareData = require('./prepareData')
+const groupWords = require('./groupWords')
 ipcMain.on('process-subtitle', (event, paths) => {
-    pathsToRows(paths).then(rows => console.log(rows)).then(() => {
-        event.reply('process-subtitle', 'pong')
-    })
+    pathsToRows(paths)
+        .then(rows => prepareData(rows))
+        .then(words => groupWords(words))
+        .then(groupedWords => {
+            event.reply('process-subtitle', groupedWords)
+        })
 
     
 })
